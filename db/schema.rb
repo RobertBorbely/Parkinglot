3257901aef2f60ed7ce0c1833378b73c6d4b5846
RobertBorbely <respectsas@gmail.com>
2015-05-13 23:40:04 +0200

    Layout making

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150508181121) do
+ActiveRecord::Schema.define(version: 20150513203828) do
 
   create_table "car_images", force: :cascade do |t|
     t.string   "image_file_name"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 20150508181121) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
